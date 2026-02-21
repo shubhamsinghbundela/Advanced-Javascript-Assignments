248ae5885dbd0e0e4aee070589bf9c3654b8b2dd
shubhamsinghbundela <shubhamkrsingh11@gmail.com>
2026-02-21 07:10:38 +0530

    retryOnce.js 21-02-2026

--- callbacks/easy/retryOnce.js
+++ callbacks/easy/retryOnce.js
@@ -6,8 +6,37 @@
 // If the second attempt also rejects, the error should be propagated.
 
 
-function retryOnce(fn) {
+//given a function `fn` that returns a Promise. (It means this fn is callback-based asynchronous function)
+//  let calls = 0;
 
+//     const fn = (cb) => {
+//       calls++;
+//       if (calls === 1) {
+//         cb("fail", null);
+//       } else {
+//         cb(null, "success");
+//       }
+//     };
+
+//     const wrapped = retryOnce(fn);
+
+function retryOnce(fn) {
+    return function (...args) {
+        const callback = args.pop();
+        fn(...args, (err,data)=>{
+            if(err){
+                fn(...args, (err,data)=>{
+                    if(err){
+                        callback(err, data);
+                    }else{
+                      callback(err, data);  
+                    }
+                })
+            }else{
+                callback(err, data);
+            }
+        })
+    }
 }
 
 module.exports = retryOnce;
