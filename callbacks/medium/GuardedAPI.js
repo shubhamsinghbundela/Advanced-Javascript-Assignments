765efb95c2fd43d6114450432952dd105e4db5fe
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-03-04 14:17:27 +0530

    guardedAPI medium problem 04-03-2026

--- callbacks/medium/GuardedAPI.js
+++ callbacks/medium/GuardedAPI.js
@@ -14,12 +14,16 @@ class GuardedAPI {
   }
 
   init(initTask) {
+    //asynchronous initialization
     this.initTask = initTask;
   }
 
   call(apiFn, onComplete) {
+     // Any calls made before initialization completes should be queued an executed only after the initialization finishes.
     this.queue.push({apiFn, onComplete});
     this.initTask((err, data)=>{
+      // APIs that depend on an asynchronous initialization step.
+      // The initialization task 
       this._flush();
     })
   }
