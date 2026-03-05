c475acf2837a52a8f2d24633ef3e903a84d6f313
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-03-05 12:30:45 +0530

    callback medium problem hedgedRequest 05-03-2026

--- callbacks/medium/hedgedRequest.js
+++ callbacks/medium/hedgedRequest.js
@@ -11,6 +11,53 @@
 // - Start Secondary after timeoutMs if needed.
 // - First success wins.
 // - Callback must be called exactly once.
-function hedgedRequest(primary, secondary, timeoutMs, onComplete) {}
+function hedgedRequest(primary, secondary, timeoutMs, onComplete) {
+  let primaryRunFast = false;
+  let secondaryRunFast = false;
+  let primaryFail = false;
+  let secondaryFail = false;
+
+  //Start the Primary immediately
+  primary((err, data) => {
+    if (data) {
+       // if secondary run fast then ignore the primary result
+      if (!secondaryRunFast) {
+        // if primary is fast then do not trigger secondary 
+        primaryRunFast = true;
+        onComplete(err, data);
+      }
+    } else {
+      primaryFail = true;
+    }
+    // Only fail if both fail
+    if (primaryFail & secondaryFail) {
+      let err = {
+        message: "Secondary Failed",
+      };
+      onComplete(err);
+    }
+  });
+  setTimeout(() => {
+    // Secondary should not be triggered if Primary is fast
+    if (!primaryRunFast) {
+        // If primary is slow, start the Secondary.
+      secondary((err, data) => {
+        if (data) {
+          secondaryRunFast = true;
+          onComplete(err, data);
+        } else {
+          secondaryFail = true;
+        }
+        // Only fail if both fail
+        if (primaryFail & secondaryFail) {
+          let err = {
+            message: "Secondary Failed",
+          };
+          onComplete(err);
+        }
+      });
+    }
+  }, timeoutMs);
+}
 
 module.exports = hedgedRequest;
