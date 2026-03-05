0c8d971f59a97780e1f0715105435a125370bde1
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-03-05 14:38:57 +0530

    runWithDependecises problem 05-03-2026

--- callbacks/medium/runWithDependencies.js
+++ callbacks/medium/runWithDependencies.js
@@ -11,6 +11,37 @@
 // Invoke finalCallback after all tasks have completed, or with an error
 // if any task fails.
 
-function runWithDependencies(tasks, finalCallback) {}
+function runWithDependencies(tasks, finalCallback) {
+  let queue = [];
+  for (let i = 0; i < tasks.length; i++) {
+    if(tasks[i].deps.length>0){
+        tasks[i].deps.forEach(element => {
+             let findTask = tasks.find(e => e.id == element);
+             queue.push(findTask)
+        });
+        queue.push(tasks[i])
+    }else{
+      queue.push(tasks[i])
+    }
+  }
+  let err = false;
+  // let active = 0;
+  let obj = {};
+
+  for (let i = 0; i < queue.length; i++) {
+    const element = queue[i];
+    queue[i].run((err, data)=>{
+      if(err){
+        err=true;
+      }else{
+        // arr.push({[element.id]: data})
+        obj[element.id] = data
+      }
+      if(i==queue.length-1){
+        finalCallback(err, obj)
+      }
+    })
+  }
+}
 
 module.exports = runWithDependencies;
