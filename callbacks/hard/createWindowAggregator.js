f719ee3222ec4b5ed9158f3dc64e6b69fac96835
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-03-11 07:51:27 +0530

    create window aggregator

--- callbacks/hard/createWindowAggregator.js
+++ callbacks/hard/createWindowAggregator.js
@@ -14,7 +14,38 @@
 // - On each new value, compute and emit the current average.
 // - Before N values are received, compute the average
 //   using only the available values.
-function createWindowAggregator(windowSize, onWindowReady) {}
+function createWindowAggregator(windowSize, onWindowReady) {
+  let arr = [];
+  let l=0, r=windowSize-1;
+  return function(...args){
+     //On each new value, compute and emit the current average.
+     arr.push(...args);
+     //Before N values are received, compute the average
+     if(arr.length<windowSize){
+        let sum = 0;
+        for(let i=0; i<arr.length;i++){
+            sum+=arr[i];
+        }
+
+        let avg = sum/arr.length;
+        onWindowReady(avg);
+     }else{
+       while(r<arr.length){
+        // console.log(r);
+          let sum = 0;
+          for(let i=l; i<=r; i++){
+            sum+=arr[i];
+          }
+          console.log(sum);
+
+          let avg =  sum/windowSize;
+          onWindowReady(avg);
+          l+=1;
+          r+=1;
+       }
+     }
+  }
+}
 
 module.exports = createWindowAggregator;
 
