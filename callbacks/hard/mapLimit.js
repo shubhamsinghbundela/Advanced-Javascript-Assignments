ed6c801aaff8aa69d0721d0e3af88ca530dcbbd0
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-03-18 11:21:41 +0530

    mapLimit problem solve 18-03-2026

--- callbacks/hard/mapLimit.js
+++ callbacks/hard/mapLimit.js
@@ -11,6 +11,33 @@
 // - Run at most `limit` tasks in parallel until all are completed.
 // - Return results in the original task order via onAllFinished.
 
-function mapLimit(tasks, limit, onAllFinished) {}
+function mapLimit(tasks, limit, onAllFinished) {
+    let active = 0;
+    let results = [];
+    let index = 0;
+    let completed = 0;
+
+    function next(){
+        
+        if (completed === tasks.length) {
+          return onAllFinished(null, results);
+        }
+         while (active < limit && index < tasks.length) {
+            const currentIndex = index;
+            active++;
+            index++;
+            const task = tasks[currentIndex];
+            task((err, data)=>{
+                
+                results[currentIndex] = data;
+                active--;
+                completed++;
+
+                next();
+            })
+        }
+    }
+    next();
+}
 
 module.exports = mapLimit;
