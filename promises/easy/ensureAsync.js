1e37c2d0cd7a1980310bd6f992892f0bce9d4422
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-04-10 00:15:21 +0530

    solved ensureAsync 09-04-2026

--- promises/easy/ensureAsync.js
+++ promises/easy/ensureAsync.js
@@ -1,16 +1,18 @@
 // Problem Description – ensureAsync(fn)
 
-// You are required to write a function named ensureAsync that takes another function fn as input. 
+// You are required to write a function named ensureAsync that takes another function fn as input.
 // The goal is to guarantee that calling fn always returns a Promise, even if fn is synchronous.
 // Using the async keyword is recommended, as it automatically wraps return values and errors in a Promise.
 function ensureAsync(fn) {
-    return function(...args) {
-        try {
-            return Promise.resolve(fn(...args));
-        } catch (error) {
-            return Promise.reject(error);
-        }
-    }
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(fn(...args));
+      } catch (err) {
+        reject(err);
+      }
+    });
+  };
 }
 
 module.exports = ensureAsync;
